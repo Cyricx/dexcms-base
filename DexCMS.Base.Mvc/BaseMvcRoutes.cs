69b298968479f743cb50afeca47aaefa9904d3a2
Cyricx <contact@chrisbyram.com>
2016-06-21 21:53:20 -0500

    Added base routes and removed error controller

---
+++ DexCMS.Base.Mvc/BaseMvcRoutes.cs
@@ -0,0 +1,73 @@
+﻿using DexCMS.Base.Mvc.Enums;
+using DexCMS.Core.Infrastructure.Models;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DexCMS.Base.Mvc
+{
+    public static class BaseMvcRoutes
+    {
+        public static void CreateDefaultRoutes(RouteCollection routes, DexCMSConfiguration config)
+        {
+            routes.MapRoute("sitemap", "sitemap.xml", new { controller = "Sitemap", action = "Index" });
+
+            routes.MapRoute(
+                name: "Contact",
+                url: "contact",
+                defaults: new { category = "none", controller = "Contact", action = "Index", urlSegment = "contact" }
+            );
+
+            routes.MapRoute(
+                name: "ContactSuccess",
+                url: "contact/success",
+                defaults: new { category = "contact", controller = "Contact", action = "Success", urlSegment = "success" }
+            );
+
+            if (!config.RetrieveValue<bool>(BaseRouteOptions.DisableAccountRoutes.ToString()))
+            {
+                routes.MapRoute(
+                    name: "Account",
+                    url: "Account/{action}",
+                    defaults: new { category = "account", controller = "Account" });
+
+                routes.MapRoute(
+                    name: "Manage",
+                    url: "Manage",
+                    defaults: new { category = "none", controller = "Manage", action = "Index", urlSegment = "manage" });
+
+
+                routes.MapRoute(
+                    name: "ManageActions",
+                    url: "Manage/{action}",
+                    defaults: new { category = "manage", controller = "Manage" });
+            }
+
+            //root page
+            routes.MapRoute(
+                name: "Default",
+                url: "",
+                defaults: new { urlSegment = "index", controller = "PublicContent", action = "Index" }
+            );
+
+            //THESE are the catch all routes that display dynamic content
+            routes.MapRoute(
+                name: "Content",
+                url: "{urlSegment}",
+                defaults: new { controller = "PublicContent", action = "RetrieveContent" }
+            );
+
+            routes.MapRoute(
+                name: "ContentCategory",
+                url: "{category}/{urlSegment}",
+                defaults: new { controller = "PublicContent", action = "RetrieveContentByCategory" }
+            );
+
+            routes.MapRoute(
+                name: "ContentSubCategory",
+                url: "{category}/{subCategory}/{urlSegment}",
+                defaults: new { controller = "PublicContent", action = "RetrieveContentBySubCategory" }
+            );
+        }
+    }
+
+}
--- DexCMS.Base.Mvc/Enums/BaseRouteOptions.cs
+++ DexCMS.Base.Mvc/Enums/BaseRouteOptions.cs
@@ -3,8 +3,6 @@ namespace DexCMS.Base.Mvc.Enums
 {
     public enum BaseRouteOptions
     {
-        DisableLowercaseUrls,
-        MvcIgnoreRoutes,
         DisableAccountRoutes
     }
 }
