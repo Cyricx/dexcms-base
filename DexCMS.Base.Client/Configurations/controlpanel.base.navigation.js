af40d12557795c6e32cd5cf33ddb5f051949b4bb
Cyricx <contact@chrisbyram.com>
2016-07-30 16:30:49 -0500

    Added client routes and navigation

---
+++ DexCMS.Base.Client/Configurations/controlpanel.base.navigation.js
@@ -0,0 +1,77 @@
+﻿module.exports = [
+    {
+        "title": "Contact",
+        "icon": "fa-envelope",
+        "subLinks": [
+            {
+                "title": "Messages",
+                "href": "contacts"
+            },
+            {
+                "title": "Contact Types",
+                "href": "contacttypes"
+            }
+        ]
+    },
+    {
+        "title": "Pages",
+        "icon": "fa-newspaper-o",
+        "subLinks": [
+            {
+                "title": "Page Contents",
+                "href": "pagecontents"
+            },
+            {
+                "title": "Layout Types",
+                "href": "layouttypes"
+            },
+            {
+                "title": "Page Types",
+                "href": "pagetypes"
+            },
+            {
+                "title": "Areas",
+                "href": "contentareas"
+            },
+            {
+                "title": "Categories",
+                "href": "contentcategories"
+            },
+            {
+                "title": "Sub Categories",
+                "href": "contentsubcategories"
+            }
+        ]
+    },
+
+    {
+        "title": "Settings",
+        "icon": "fa-cogs",
+        "subLinks": [
+            {
+                "title": "Countries",
+                "href": "countries"
+            },
+            {
+                "title": "Images",
+                "href": "images"
+            },
+            {
+                "title": "States",
+                "href": "states"
+            },
+            {
+                "title": "Settings",
+                "href": "settings"
+            },
+            {
+                "title": "Setting DataTypes",
+                "href": "settingdatatypes"
+            },
+            {
+                "title": "Setting Groups",
+                "href": "settinggroups"
+            }
+        ]
+    }
+];
---
+++ DexCMS.Base.Client/Configurations/controlpanel.base.routes.js
@@ -0,0 +1,104 @@
+﻿module.exports = [
+    {
+        "name": "contacts",
+        "module": "base",
+        "routes": [
+            {
+                "viewType": "list",
+                "path": ""
+            },
+            {
+                "viewType": "details",
+                "path": "/details/:id"
+            }
+        ]
+    },
+    {
+        "name": "contactTypes",
+        "module": "base"
+    },
+    {
+        "name": "contentBlocks",
+        "module": "base",
+        "routes": [
+            {
+                "viewType": "editor",
+                "path": "/:id"
+            },
+            {
+                "viewType": "editor",
+                "path": "/:id?fromadmin"
+            },
+            {
+                "viewType": "editor",
+                "path": "/new/:parentId"
+            },
+            {
+                "viewType": "editor",
+                "path": "/new/:parentId?fromadmin"
+            }
+        ]
+    },
+    {
+        "name": "contentAreas",
+        "module": "base"
+    },
+    {
+        "name": "contentCategories",
+        "module": "base"
+    },
+    {
+        "name": "contentSubCategories",
+        "module": "base"
+    },
+    {
+        "name": "pageTypes",
+        "module": "base"
+    },
+    {
+        "name": "layoutTypes",
+        "module": "base"
+    },
+    {
+        "name": "countries",
+        "module": "base"
+    },
+    {
+        "name": "states",
+        "module": "base"
+    },
+    {
+        "name": "images",
+        "module": "base"
+    },
+    {
+        "name": "pageContents",
+        "module": "base"
+    },
+    {
+        "name": "pageContentImages",
+        "module": "base",
+        "routes": [
+            {
+                "viewType": "list",
+                "path": "/:id"
+            },
+            {
+                "viewType": "list",
+                "path": "/:id?fromadmin"
+            }
+        ]
+    },
+    {
+        "name": "settingDataTypes",
+        "module": "base"
+    },
+    {
+        "name": "settingGroups",
+        "module": "base"
+    },
+    {
+        "name": "settings",
+        "module": "base"
+    },
+];
