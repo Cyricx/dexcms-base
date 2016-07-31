d837ae3419cd0cd5a77dafdd62458b99a05b45bc
Cyricx <contact@chrisbyram.com>
2016-07-30 22:48:50 -0500

    Added missing dest for global base settigs

--- DexCMS.Base.Client/Configurations/globals.base.settings.js
+++ DexCMS.Base.Client/Configurations/globals.base.settings.js
@@ -1,6 +1,7 @@
 ﻿module.exports = function (appPath) {
     return {
         name: 'GlobalsBaseSettings',
+        dest: appPath + '/globals/base/config/dexcms.globals.base.settings.json',
         options: {
             startingRoute: '../../../' + appPath + '/globals/base/'
         }
