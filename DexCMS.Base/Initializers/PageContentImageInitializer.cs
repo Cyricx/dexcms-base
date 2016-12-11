c693b5d49e80d6e92b4fd00ae268fb13dc955cab
Cyricx <contact@chrisbyram.com>
2016-12-11 07:00:24 -0600

    Added display order to default content images

--- DexCMS.Base/Initializers/PageContentImageInitializer.cs
+++ DexCMS.Base/Initializers/PageContentImageInitializer.cs
@@ -21,20 +21,20 @@ namespace DexCMS.Base.Initializers
         public override void Run()
         {
             Context.PageContentImages.AddIfNotExists(x => new { x.PageContentID, x.ImageID },
-                new PageContentImage { PageContentID = Contents.LeftSidebar, ImageID = Images.ImageOne },
-                new PageContentImage { PageContentID = Contents.LeftSidebar, ImageID = Images.ImageTwo },
-                new PageContentImage { PageContentID = Contents.LeftSidebarOnly, ImageID = Images.ImageOne },
-                new PageContentImage { PageContentID = Contents.LeftSidebarOnly, ImageID = Images.ImageTwo },
-                new PageContentImage { PageContentID = Contents.OneColumn, ImageID = Images.ImageOne },
-                new PageContentImage { PageContentID = Contents.OneColumn, ImageID = Images.ImageTwo },
-                new PageContentImage { PageContentID = Contents.RightSidebar, ImageID = Images.ImageOne },
-                new PageContentImage { PageContentID = Contents.RightSidebar, ImageID = Images.ImageTwo },
-                new PageContentImage { PageContentID = Contents.RightSidebarOnly, ImageID = Images.ImageOne },
-                new PageContentImage { PageContentID = Contents.RightSidebarOnly, ImageID = Images.ImageTwo },
-                new PageContentImage { PageContentID = Contents.ThreeColumn, ImageID = Images.ImageOne },
-                new PageContentImage { PageContentID = Contents.ThreeColumn, ImageID = Images.ImageTwo },
-                new PageContentImage { PageContentID = Contents.TwoColumn, ImageID = Images.ImageOne },
-                new PageContentImage { PageContentID = Contents.TwoColumn, ImageID = Images.ImageTwo }
+                new PageContentImage { PageContentID = Contents.LeftSidebar, ImageID = Images.ImageOne, DisplayOrder = 0 },
+                new PageContentImage { PageContentID = Contents.LeftSidebar, ImageID = Images.ImageTwo, DisplayOrder = 1 },
+                new PageContentImage { PageContentID = Contents.LeftSidebarOnly, ImageID = Images.ImageOne, DisplayOrder = 0 },
+                new PageContentImage { PageContentID = Contents.LeftSidebarOnly, ImageID = Images.ImageTwo, DisplayOrder = 1 },
+                new PageContentImage { PageContentID = Contents.OneColumn, ImageID = Images.ImageOne, DisplayOrder = 0 },
+                new PageContentImage { PageContentID = Contents.OneColumn, ImageID = Images.ImageTwo, DisplayOrder = 1 },
+                new PageContentImage { PageContentID = Contents.RightSidebar, ImageID = Images.ImageOne, DisplayOrder = 0 },
+                new PageContentImage { PageContentID = Contents.RightSidebar, ImageID = Images.ImageTwo, DisplayOrder = 1 },
+                new PageContentImage { PageContentID = Contents.RightSidebarOnly, ImageID = Images.ImageOne, DisplayOrder = 0 },
+                new PageContentImage { PageContentID = Contents.RightSidebarOnly, ImageID = Images.ImageTwo, DisplayOrder = 1 },
+                new PageContentImage { PageContentID = Contents.ThreeColumn, ImageID = Images.ImageOne, DisplayOrder = 0 },
+                new PageContentImage { PageContentID = Contents.ThreeColumn, ImageID = Images.ImageTwo, DisplayOrder = 1 },
+                new PageContentImage { PageContentID = Contents.TwoColumn, ImageID = Images.ImageOne, DisplayOrder = 0 },
+                new PageContentImage { PageContentID = Contents.TwoColumn, ImageID = Images.ImageTwo, DisplayOrder = 1 }
             );
         }
 
