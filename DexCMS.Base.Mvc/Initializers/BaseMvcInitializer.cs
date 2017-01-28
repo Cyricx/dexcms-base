e8f188a2dae7e0ce049cd7c7a6d561583b1b0651
Cyricx <contact@chrisbyram.com>
2017-01-28 12:44:19 -0600

    Added default mvc initializers

---
+++ DexCMS.Base.Mvc/Initializers/BaseMvcInitializer.cs
@@ -0,0 +1,20 @@
+﻿using DexCMS.Base.Contexts;
+using DexCMS.Core.Globals;
+using System;
+using System.Collections.Generic;
+
+namespace DexCMS.Base.Mvc.Initializers
+{
+    public class BaseMvcInitializer: DexCMSLibraryInitializer<IDexCMSBaseContext>
+    {
+        public BaseMvcInitializer(IDexCMSBaseContext context) : base(context) { }
+
+        public override List<Type> Initializers
+        {
+            get
+            {
+                return new List<Type>();
+            }
+        }
+    }
+}
